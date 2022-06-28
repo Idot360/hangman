e00b9a4364cf441804aa644c00e8627dcb1d78e1
Idot360 <evan.h@outlook.com>
2022-06-28 10:42:20 +0800

    idk

--- hangman.py
+++ hangman.py
@@ -5,56 +5,146 @@ import csv
 import random
 
 
+
+
 def generate_word(filename="Hangman 4K Database.csv"):
-    
     """
-    Inputs:
-      filename  - name of CSV file where each row contains characters that
-                  form a single word
-    Output:
-      Returns a string containing a single word
+    generates a random word from a csv file
+
+    Parameters
+    ----------
+    filename : string
+        name of CSV file where each row contains characters that
+        form a single word
+
+    Returns
+    -------
+    random_word : string
+        a word from the csv file provided
+
     """
     
     with open("Hangman 4K Database.csv") as csvfile:
         data = csv.reader(csvfile)
         words_list = list()
         for row in data:
-            words_list.append(row)
+            words_list.extend(row)
     
     random_word = random.choice(words_list)
-    return random_word
+    return random_word.upper()
 
 
 
 
-'''
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-'''
+def prompt_guess(used_letters):
+    '''
+    prompts the user to enter an character and checks if string 
+    is an used single character and part of the alphabet
 
-def test_generate_word(limit):
+    Returns
+    -------
+    guess : a string containing a single uppercase character
+
+    '''
     
-    """
-    Inputs:
-      limit - an integer within 0-1000 that denotes how many times the
-              function generate_word will be ran
-    Output:
-      None
-      
-    Will cause an error if generate_word is found to fail
-    """
-    for i in range(int(limit)):
-        word = generate_word("Hangman 4K Database.csv")
-        words_list = list()
-        with open("Hangman 4K Database.csv") as csvfile:
-            data = csv.reader(csvfile)
-            for row in data:
-                words_list.append(row)
-        assert word in words_list
+    invaild = True
+    while invaild:
+        guess = input("\nPlease enter a single unused alphabet as your guess: ")
+        invaild = guess.upper() in used_letters or len(guess) != 1 or not guess.isalpha()
+    return guess.upper()
 
 
-test_generate_word(1)
+
+
+def generate_incomplete_word(original_word, guessed_letters):
+    '''
+    
+
+    Parameters
+    ----------
+    original_word : TYPE
+        DESCRIPTION.
+    guessed_letters : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    word : TYPE
+        DESCRIPTION.
+
+    '''
+    
+    word = str()
+    for alphabet in original_word:
+        if alphabet in guessed_letters:
+            word = word + alphabet
+        else:
+            word = word + '_ '
+    return word
+
+
+
+
+def main():
+    '''
+    
+
+    Returns
+    -------
+    None.
+
+    '''
+    
+    input('''Welcome to hangman\nPress enter to start''')
+    incorrect_guesses = int(0)
+    original_word = generate_word()
+    word = original_word
+    used_letters = str()
+    guessed_letters = str()
+    guess = str()
+    
+    while incorrect_guesses < 7:
+
+        word = generate_incomplete_word(guessed_letters, original_word)
+        
+        
+        if word.isalpha():
+            print(f"You got the word.\nThe word was {word}")
+            return
+        
+        
+        print(f'''
+----------------------------------------------------------------------------
+\nYou have {incorrect_guesses} incorrect_guesses.''')
+        print("Word:", word, "\n"*40)
+        
+        
+        guess = prompt_guess(used_letters)
+        if guess in original_word:
+            print(f"There is {guess} in the word")
+            guessed_letters = guessed_letters + guess
+        else:
+            print(f"There is no {guess} in the word")
+            incorrect_guesses += 1
+        used_letters = used_letters + guess
+        
+        
+        
+    print('''\n\nYou ran out of guesses.\nThe word was''', original_word)
+
+
+
+
+
+main()
+
+
+'''
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+'''
+
 
 
 
